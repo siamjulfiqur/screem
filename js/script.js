01fd1a368de76991155a3a767cf082a34513a0f1
siamjulfiqur <142520460+siamjulfiqur@users.noreply.github.com>
2024-03-26 12:36:11 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function dark(){
+    document.querySelector('html').style.
+    colorScheme = 'dark'
+}
+
+function light(){
+    document.querySelector('html').style.
+    colorScheme = 'light'
+}
